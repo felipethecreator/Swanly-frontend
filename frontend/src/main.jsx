a0b6f47f9d31c302ead80738a774bd819fb34b58
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-09-15 14:42:18 -0300

    feat: Alterando imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App';
-import './style/main.css'
+import './main.css'
 
 createRoot(document.getElementById('root')).render(<App/>);
