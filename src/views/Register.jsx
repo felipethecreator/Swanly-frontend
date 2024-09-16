d9c3a7a453b225c7c2dc6191c0c6942426b64f60
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-09-16 10:08:10 -0300

    feat: acrescentando axios ao projeto e fazendo integração back x front

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,16 +1,59 @@
-import '../style/Register.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../style/Register.css';
 
 export default function Register() {
-    return(
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:3000/register', {
+                email,
+                username,
+                password,
+            });
+
+            console.log(response.data);
+        } catch (error) {
+            console.error('Erro ao registrar usuário:', error);
+        }
+    };
+
+    return (
         <div id='register-div'>
             <h1>Javex Feed</h1>
-            <form id='register-form'>
+            <form id='register-form' onSubmit={handleSubmit}>
                 <h2>Registre-se</h2>
-                <input type="email" className="register-inputs" placeholder='Email' required/>
-                <input type="text" className="register-inputs" placeholder='Username' required/>
-                <input type="password" className="register-inputs" placeholder='Senha' required/>
+                <input
+                    type="email"
+                    className="register-inputs"
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <input
+                    type="text"
+                    className="register-inputs"
+                    placeholder='Username'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    className="register-inputs"
+                    placeholder='Senha'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
                 <button type='submit' id="register-button">Registrar</button>
             </form>
         </div>
-    )
+    );
 }
