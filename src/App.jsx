57e112d12145fee6c6339f15ba922356e0e0e73b
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-10-25 10:06:43 -0300

    feat: add new LandingPage page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import RegisterPage from "./pages/Register"
+import LandingPage from "./pages/LandingPage"
 
 export default function App() {
 
   return(
-     <RegisterPage/> 
+     <LandingPage/> 
   )
 }
---
+++ src/pages/LandingPage/index.jsx
@@ -0,0 +1,10 @@
+import Header from '../../components/Header';
+
+export default function LandingPage() {
+    return (
+    <>
+        <Header/>
+        <p>oiiii</p>
+    </>
+    );
+}
