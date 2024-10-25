7fd9ad00bab92fd8653603f8cd8f7d114d582f40
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-10-25 10:37:52 -0300

    fix: resolve header overlap issue

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,9 +1,26 @@
+import './style.css'
 import Header from '../../components/Header';
 
 export default function LandingPage() {
     return (
     <>
         <Header/>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+        <p>OII</p>
+
     </>
     );
 }
